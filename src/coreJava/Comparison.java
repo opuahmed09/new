383229915d9da62526e3f411f752b3c9ffbe0ecc
mrahman <rahmanww@gmail.com>
2019-04-03 21:17:59 -0400

    added comparison class with if else statement

---
+++ src/coreJava/Comparison.java
@@ -0,0 +1,48 @@
+package coreJava;
+
+public class Comparison {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		
+		int c = 7;
+		int d = 8;
+		
+		
+		//if a = b 
+		//print a is equal to b
+		if(a == b) 
+			System.out.println("a is equal to b");
+		
+		//if a is not = b 
+		//print a is not equal to b
+
+		if(c != d) 
+			System.out.println("c is not equal to d");
+		
+		//if a is greater than b 
+		//print a is greater than b
+		
+		if(a>b)
+			System.out.println("a is greater than b");
+
+		//if a is less than c 
+		//print a is less than c
+				
+		if(c<a)
+		System.out.println("c is less than a");
+		
+		//if a = b 
+		//print a is equal to b
+		//else a is not = b
+		//print a is not equal to b
+		
+		if(a == b) {
+			System.out.println("new : a is equal to b");
+		}else {
+			System.out.println("new : a is not equal to b");
+		}
+	}
+
+}
